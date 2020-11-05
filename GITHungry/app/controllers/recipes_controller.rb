2979f021ed5944084ee45569821ef0e6794a025c
JinaStrickland <Jina30@gmail.com>
2020-11-05 16:33:57 -0500

    filled out controllers

--- GITHungry/app/controllers/recipes_controller.rb
+++ GITHungry/app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+
+    def index
+        render json: Recipe.all 
+    end
+
+    def show 
+        render json: find_recipe
+    end
+
+    def create 
+        recipe = Recipe.create(recipe_params)
+        render json: recipe
+    end
+
+    def update 
+        recipe.update(find_recipe)
+        render json: recipe
+    end
+
+    def destroy 
+        recipe.destroy 
+        render json: {message: "Recipe has been deleted"}
+    end
+
+    private
+
+    def find_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:ingredients, :instructions, :cuisine_type, :cooking_time, :tag, :user_ratings, :servings, :user_comments, :image)
+    end
+    
 end
--- GITHungry/app/controllers/users_controller.rb
+++ GITHungry/app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all 
+    end
+
+    def show 
+        render json: find_user
+    end
+
+    def create 
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def update 
+        recipe.update(find_user)
+        render json: user
+    end
+
+    def destroy 
+        user.destroy 
+        render json: {message: "User has been deleted"}
+    end
+
+    private
+
+    def find_recipe
+        @user = User.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:user).permit(:username, :email)
+    end
+
 end
